954d7770280c8b019933a87184857e642818a642
Marcos Pereira <marcos.si.pereira@gmail.com>
2025-03-06 09:02:21 -0300

    Add export of petro-app.

--- packages/rmf-dashboard-framework/src/micro-apps/index.ts
+++ packages/rmf-dashboard-framework/src/micro-apps/index.ts
@@ -3,4 +3,5 @@ export { default as liftsApp } from './lifts-app';
 export { default as createMapApp } from './map-app';
 export { default as robotMutexGroupsApp } from './robot-mutex-groups-app';
 export { default as robotsApp } from './robots-app';
+export { default as petroApp } from './petro-app';
 export { default as tasksApp } from './tasks-app';
